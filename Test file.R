7fd49e8c9d216988908585cda2a70eb555e4d4f1
luweej <126919630+luweej@users.noreply.github.com>
2023-03-05 09:38:21 -0500

    commit

--- Test file.R
+++ Test file.R
@@ -7,3 +7,10 @@
 # abc testb
 # new test 3/5
 #abc def test
+gpa <- c(3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 4.7)
+
+age <- c(21, 22, 23, 24, 25, 26, 27)
+
+wt <- c(151, 142, 133, 164, 165, 166, 167)
+
+df <- data.frame(gpa, age, wt)
